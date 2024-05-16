c33a5774b5402d72873e1bb76faa23602484240d
starling188 <gonzalezmiranda188@gmail.com>
2024-05-16 14:22:47 -0400

    Agregando todo

---
+++ ConsoleApp1/ConsoleApp1/Entities/FiguraGeometrica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entities
+{
+    public  class FiguraGeometrica
+    {
+        public double Base { get; set; }
+
+        public double Altura { get; set; }
+
+        public virtual double CalcularArea()
+        {
+            return Base * Altura;
+        }
+    }
+}
